9466d53b49d8e127db31757a2fa5134f770f61e5
Alejandro Rodriguez <alejanmrodriguez@gmail.com>
2022-06-24 00:23:24 -0400

    Refactored Game component to be reusable for other images, added functionality to check for correct answers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Game from "./components/Game";
+import disneyland from "./images/wheres-mickey.jpg";
 
 function App() {
     return (
         <div className="App">
-            <Game />
+            <Game
+                src={disneyland}
+                characters={[
+                    { name: "Mickey", x: 843, y: 291 },
+                    { name: "Minnie", x: 69, y: 257 },
+                    { name: "Donald", x: 418, y: 406 },
+                    { name: "Daisy", x: 612, y: 495 },
+                    { name: "Goofy", x: 670, y: 243 },
+                    { name: "Pluto", x: 218, y: 494 }
+                ]}
+            />
         </div>
     );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,6 @@
 // TODO Change out mickey specific for props
 
 import React, { useState } from "react";
-import mickey from "../images/wheres-mickey.jpg";
 
 function Game(props) {
     const [characterSelect, setCharacterSelect] = useState({
@@ -10,6 +9,12 @@ function Game(props) {
         display: false
     });
 
+    const [foundStatus, setFoundStatus] = useState(
+        props.characters.map(character => {
+            return { name: character.name, found: false };
+        })
+    );
+
     function displayCharacterSelect(e) {
         if (characterSelect.display) {
             setCharacterSelect({ x: 0, y: 0, display: false });
@@ -17,8 +22,33 @@ function Game(props) {
             setCharacterSelect({
                 x: e.clientX,
                 y: e.clientY,
+                xAnswer: e.nativeEvent.offsetX,
+                yAnswer: e.nativeEvent.offsetY,
                 display: true
             });
+        }
+    }
+
+    function checkAnswer(e) {
+        const name = e.target.textContent;
+        const { x: correctX, y: correctY } = props.characters.find(
+            character => character.name === name
+        );
+        console.log({
+            xAnswer: characterSelect.xAnswer,
+            correctX,
+            yAnswer: characterSelect.yAnswer,
+            correctY
+        });
+        if (
+            correctX - 25 <= characterSelect.xAnswer &&
+            characterSelect.xAnswer <= correctX + 25 &&
+            correctY - 25 <= characterSelect.yAnswer &&
+            characterSelect.yAnswer <= correctY + 25
+        ) {
+            console.log("Correct!");
+        } else {
+            console.log("Wrong!");
         }
     }
 
@@ -27,18 +57,23 @@ function Game(props) {
             <div className="image-container">
                 <img
                     onClick={e => displayCharacterSelect(e)}
-                    src={mickey}
+                    src={props.src}
                     alt="A where's waldo game involving Mickey and friends at Disneyland."
                 />
             </div>
+            {/* Key listing characters to be found */}
             <ul className="answer-key">
                 <p>Can you find...?</p>
-                <li>Mickey</li>
-                <li>Minnie</li>
-                <li>Donald</li>
-                <li>Daisy</li>
-                <li>Goofy</li>
-                <li>Pluto</li>
+                {foundStatus.map((character, index) => {
+                    return (
+                        <div
+                            className={character.found ? "found" : undefined}
+                            key={index}
+                        >
+                            {character.name}
+                        </div>
+                    );
+                })}
             </ul>
             {characterSelect.display && (
                 <div
@@ -49,14 +84,22 @@ function Game(props) {
                         left: characterSelect.x - 25
                     }}
                 >
+                    {/* Box highlighting selected area when image is clicked */}
                     <div className="selected-area"></div>
+                    {/* Display characters not yet found to choose from for answer */}
                     <div className="select-character">
-                        <div>Mickey</div>
-                        <div>Minnie</div>
-                        <div>Donald</div>
-                        <div>Daisy</div>
-                        <div>Goofy</div>
-                        <div>Pluto</div>
+                        {foundStatus
+                            .filter(character => !character.found)
+                            .map((character, index) => {
+                                return (
+                                    <div
+                                        onClick={e => checkAnswer(e)}
+                                        key={index}
+                                    >
+                                        {character.name}
+                                    </div>
+                                );
+                            })}
                     </div>
                 </div>
             )}
@@ -66,9 +109,9 @@ function Game(props) {
 
 export default Game;
 
+// Mickey: 843, 291
 // Minnie: 69, 257
 // Donald: 418, 406
-// Daisy: 612., 495
-// Mickey: 843, 291
-// Pluto: 218, 494
+// Daisy: 612, 495
 // Goofy: 670, 243
+// Pluto: 218, 494
